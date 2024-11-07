081df13c3a43d020c6fdc2da116bbe27f45af62b
Alabama Aldi Employee <164882065+PointerToObject@users.noreply.github.com>
2024-11-07 09:49:32 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,13 @@
 #include <cstdio>
 #include "mem.h"
 
-// ac_client.exe+637E9 dec esi instruction
-// ac_client.exe+637E9 - FF 0E                
-
 int main()
 {
 	Attach(L"ac_client.exe", L"ac_client.exe");
-	uintptr_t base = AttachModuleBase();
-	uintptr_t entPtr = base + 0x10F4F4;
-	uintptr_t ent = RPC(entPtr, { 0x0 });
-	int ammo = ent + 0x150;
-	bool on = false;
+	int addr = 0x0086A5B8; //dynamic 
 	while (true)
 	{
-		if (GetAsyncKeyState(VK_NUMPAD1) & 1)
-		{
-			on = !on;
-		}
-		if (on)
-		{
-			Nop((BYTE*)(base + 0x637E9), 2);
-			printf("[ON] - NOP (DEC ESI)");
-		}
-		else
-		{
-			Patch((BYTE*)(base + 0x637E9), (BYTE*)"\xFF\x0E", 2);
-			printf("[OFF] - NOP (DEC ESI)");
-		}
-		Sleep(25);
-		system("CLS");
+		Write<int>(addr, 500);
+		Sleep(150);
 	}
-
 }
